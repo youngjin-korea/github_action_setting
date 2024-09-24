01fd06ad94084aa21452ab288a1bcfe08dc207bc
YoungJinKim <youngjin1293@naver.com>
2024-09-24 22:01:09 +0900

    commit

---
+++ src/main/java/com/example/insta_server/InstaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.insta_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/insta_server/InstaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.insta_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
